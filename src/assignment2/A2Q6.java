29c4d1b01cd59d6859cf5eb595e336b636290777
stahc1596 <stahc1596@HRH-IC0027341.wrdsb.ca>
2017-09-18 10:10:00 -0400

    Questions 5 and 6 answers are backwards

--- src/assignment2/A2Q6.java
+++ src/assignment2/A2Q6.java
@@ -11,9 +11,20 @@ package assignment2;
 public class A2Q6 {
 
     public int convert(int n, int b){
-        
+        String s = "ABCDEF";
+        if(n == 0){
+            return 0;
+        }else{
+            if(n%b < 10){
+                System.out.print(n%b);
+            }else{
+                System.out.print(s.charAt((n%b) - 10));
+            }
+            return convert(n/b, b);
+        }
     }
     public static void main(String[] args) {
         A2Q6 test = new A2Q6();
+        int i = test.convert(1000, 16);
     }
 }
