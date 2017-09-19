253d15baa55e4d861ba05419b221caaa34388b8f
stahc1596 <stahc1596@HRH-IC0027341.wrdsb.ca>
2017-09-19 10:10:49 -0400

    Keep working on Question 6.

--- src/assignment2/A2Q5.java
+++ src/assignment2/A2Q5.java
@@ -11,20 +11,18 @@ package assignment2;
 public class A2Q5 {
 
     public int binaryConvert(int n){
-        if(n == 1){
-            return 1;
-            //Base statement
-        }else{
-            System.out.print(n%2);
-            //Gives remainder of the System
-            return binaryConvert(n/2);
-            //Divides number by two and finds the next digit in binary number
+        if(n != 1){
+            System.out.print(binaryConvert(n/2));
+            //Outputs the binary number from the method
         }
+        return n%2;
+        //Creates the Binary number to send back to the previous method
     }
     public static void main(String[] args) {
         A2Q5 test = new A2Q5();
-        test.binaryConvert(156);
-        System.out.println(1);
-        //Outputs last number of every Binary code
+        test.binaryConvert(156*2);
+        //Takes the number and multiplies it by two to get the last digit, then
+        //Sends it to the recursion method
+        
     }
 }
