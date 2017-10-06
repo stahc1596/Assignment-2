59eb2d89862f57097149ead252d404380a5aaf96
stahc1596 <stahc1596@HRH-IC0027341.wrdsb.ca>
2017-10-06 09:25:34 -0400

    fin

--- src/assignment2/A2Q7.java
+++ src/assignment2/A2Q7.java
@@ -13,10 +13,16 @@ public class A2Q7 {
     public int isPalindrom(String s, int length){
         if(s.charAt(length) != s.charAt((s.length()- 1) - length)){
             return 0;
+            //If there are two letters that aren't the same, then return that
+            //the word isn't a pallindrom
         }else if(length == (s.length() - 1) - length){
             return 1;
+            //If the system reaches the middle of the word without finding a
+            //difference in letters, then return saying it's a pallindrom
         }else{
             return isPalindrom(s, length + 1);
+            //Use recursive until two letters don't match or if the method reaches
+            //the middle of the word
         }
     }
     public static void main(String[] args) {
